4a79de4b77f8d658b521f72bea02f4780454fa78
mgoel-6788678 <mehul678678@gmail.com>
2022-04-27 00:09:50 +0530

    final touches

--- app/src/main/java/com/example/tweeklabshacktask/AthleteActivity.kt
+++ app/src/main/java/com/example/tweeklabshacktask/AthleteActivity.kt
@@ -21,6 +21,16 @@ class AthleteActivity : AppCompatActivity() {
         super.onStart()
         binding.AthleteName.text = athlete.name
         binding.AthleteSport.text = athlete.sport
+
+        var countBalls =0
+        var countsessions =0
+        for(x in athlete.sessions) {
+            countsessions++
+            countBalls += x.numActivity()!!
+        }
+
+        binding.BallsBowled.text = getString(R.string.balls, countBalls)
+        binding.NumSessions.text = getString(R.string.numSessions, countsessions)
         binding.sessionOne.text = athlete.sessions[0].toString()
         binding.sessionTwo.text = athlete.sessions[1].toString()
     }
--- app/src/main/java/com/example/tweeklabshacktask/Session.kt
+++ app/src/main/java/com/example/tweeklabshacktask/Session.kt
@@ -2,16 +2,18 @@ package com.example.tweeklabshacktask
 
 class Session {
     private var duration: String? = null
-    private var activity: String? = null
+    private var activity: Int? = null
     private var date: String? = null
 
     init {
         duration = "30 min"
-        activity = "2 Balls"
+        activity = 2
         date = "12/02/2022"
     }
 
     override fun toString(): String {
-        return "Session 1: $date\n$duration $activity"
+        return "Session 1: $date\n$duration $activity Balls"
     }
+
+    fun numActivity(): Int? { return activity}
 }
